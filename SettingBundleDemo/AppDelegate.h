ea17faf33569be0cd6a3d0ca5497b6ed7a410af9
vicky_snow <635214208@qq.com>
2016-04-11 23:08:05 +0800

    initial commit

---
+++ SettingBundleDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SettingBundleDemo
+//
+//  Created by pro on 16/4/11.
+//  Copyright © 2016年 Founder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SettingBundleDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SettingBundleDemo
+//
+//  Created by pro on 16/4/11.
+//  Copyright © 2016年 Founder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
