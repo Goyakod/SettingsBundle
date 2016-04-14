8e48b691969dbfd8111c43a8659b4d04bd9dc58f
vicky_snow <635214208@qq.com>
2016-04-14 09:32:24 +0800

    获取SettingsBundle信息

--- SettingBundleDemo/ViewController.m
+++ SettingBundleDemo/ViewController.m
@@ -16,7 +16,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //获取SettingsBundle信息
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    
+    NSLog(@"%@",[userDefaults objectForKey:@"title_perference"]);
+    NSLog(@"%@",[userDefaults objectForKey:@"sex_perference"]);
+    NSLog(@"%@",[userDefaults objectForKey:@"name_preference"]);
+    NSLog(@"%@",[userDefaults objectForKey:@"enabled_preference"]);
+    NSLog(@"%@",[userDefaults objectForKey:@"slider_preference"]);
 }
 
 - (void)didReceiveMemoryWarning {
